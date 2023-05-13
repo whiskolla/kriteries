1b29f7f1be9a52329e2d1a50f7ed11600ba68926
Anastasia <87172939+whiskolla@users.noreply.github.com>
2023-05-13 06:18:51 +0300

    Add files via upload

---
+++ LW3/LW3/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace LW3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(797, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Обший критерий";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(227, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "добавить изображение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // picture
+            // 
+            this.picture.Location = new System.Drawing.Point(12, 26);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(252, 200);
+            this.picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture.TabIndex = 2;
+            this.picture.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(290, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(480, 322);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 232);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 116);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(797, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(195, 55);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "критерий Оцу";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(940, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 31);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(797, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Значение по Оцу:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(797, 178);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(195, 48);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "критерий Ниблеска";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "formul",
+            "max",
+            "average",
+            "min"});
+            this.checkedListBox1.Location = new System.Drawing.Point(797, 232);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(195, 116);
+            this.checkedListBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1015, 492);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
---
+++ LW3/LW3/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+using OtsuThreshold;
+
+namespace LW3
+{
+    public partial class Form1 : Form
+    {
+        int it = -1, w = 0, h = 0;
+        private Bitmap image = null, image2 = null;
+        string filename1;
+        public Bitmap bitmap;
+        private Otsu ot = new Otsu();
+        private Bitmap org;
+        private Bitmap start;
+        private Bitmap average;
+        private Bitmap finish;
+        public Form1()
+        {
+            InitializeComponent();
+            image = new Bitmap(picture.Width, picture.Height);
+            picture.Image = image;
+            button1.Visible = false;
+            button3.Visible = false;
+            picture.Visible = false;
+            pictureBox2.Visible = false;
+            label1.Visible = false;
+            textBox1.Visible = false;
+            button4.Visible = false;
+        }
+
+        //добавить картинку
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.InitialDirectory = Directory.GetCurrentDirectory();
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            open.RestoreDirectory = true;
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                if (image == null)
+                {
+                    picture.Image = null;
+                    image.Dispose();
+                    
+                }
+                filename1 = open.FileName;
+                image = new Bitmap(filename1);
+                picture.Image = image;
+                org = (Bitmap)picture.Image.Clone();
+            }
+            bitmap = new Bitmap(image);
+            pictureBox2.Image = bitmap;
+            var NImg = (Bitmap)picture.Image;
+            w = NImg.Width;
+            h = NImg.Height;
+
+            start = new Bitmap(w, h);
+            average = new Bitmap(w, h);
+            finish = new Bitmap(w, h);
+
+            //Bitmap finish = new Bitmap(w, h);
+            for (int i = 0; i < h; ++i)
+            {
+                for (int j = 0; j < w; ++j)
+                {
+                    var pix = NImg.GetPixel(j, i);
+                    start.SetPixel(j, i, pix);
+                    average.SetPixel(j, i, pix);
+
+                }
+            }
+
+            button2.Visible = false;
+            button1.Visible = true;
+            button3.Visible = true;
+            picture.Visible = true;
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            button4.Visible = true;
+        }
+
+        //критерий Гаврилова
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var w = image.Width;
+            var h = image.Height;
+            int sum = 0;
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    var pix = image.GetPixel(j, i);
+                    int average = (int)(0.2125*pix.R + 0.7154*pix.G + 0.0721*pix.B);
+                    sum += average;
+                }
+            }
+            int tmp = sum / (w * h);
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    var pix = image.GetPixel(j, i);
+                    int average = (int)(pix.R + pix.B + pix.G) / 3;
+
+                    if (average <= tmp)
+                    {
+                        bitmap.SetPixel(j, i, Color.Black);
+                    }
+                    else
+                    {
+                        bitmap.SetPixel(j, i, Color.White);
+                    }
+                }
+            }
+            pictureBox2.Image = bitmap;
+        }
+
+        // критерий Отсу
+        public void CalculateBarChart()
+        {
+                // определяем размеры гистограммы. В идеале, ширина должны быть кратна 768 - 
+                // по пикселю на каждый столбик каждого из каналов
+                int width = pictureBox1.Width, height = pictureBox1.Height;
+                // получаем битмап из изображения
+                Bitmap bmp = new Bitmap(pictureBox2.Image);
+                // создаем саму гистограмму
+                Bitmap barChart = new Bitmap(width, height);
+                // создаем массивы, в котором будут содержаться количества повторений для каждого из значений каналов.
+                // индекс соответствует значению канала
+                int[] R = new int[256];
+                int[] G = new int[256];
+                int[] B = new int[256];
+                int i, j;
+                Color color;
+                // собираем статистику для изображения
+                for (i = 0; i < bmp.Width; ++i)
+                    for (j = 0; j < bmp.Height; ++j)
+                    {
+                        color = bmp.GetPixel(i, j);
+                        ++R[color.R];
+                        ++G[color.G];
+                        ++B[color.B];
+                    }
+                // находим самый высокий столбец, чтобы корректно масштабировать гистограмму по высоте
+                int max = 0;
+                for (i = 0; i < 256; ++i)
+                {
+                    if (R[i] > max)
+                        max = R[i];
+                    if (G[i] > max)
+                        max = G[i];
+                    if (B[i] > max)
+                        max = B[i];
+                }
+                // определяем коэффициент масштабирования по высоте
+                double point = (double)max / height;
+                // отрисовываем столбец за столбцом нашу гистограмму с учетом масштаба
+                for (i = 0; i < width - 3; ++i)
+                {
+                    for (j = height - 1; j > height - R[i / 3] / point; --j)
+                    {
+                        barChart.SetPixel(i, j, Color.Red);
+                    }
+                    ++i;
+                    for (j = height - 1; j > height - G[i / 3] / point; --j)
+                    {
+                        barChart.SetPixel(i, j, Color.Blue);
+                    }
+                    ++i;
+                    for (j = height - 1; j > height - B[i / 3] / point; --j)
+                    {
+                        barChart.SetPixel(i, j, Color.Green);
+                    }
+                }
+
+            pictureBox1.Image = barChart;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CalculateBarChart();
+            label1.Visible = true;
+            textBox1.Visible = true;
+            Bitmap temp = (Bitmap)org.Clone();
+            ot.Convert2GrayScaleFast(temp);
+            int otsuThreshold = ot.getOtsuThreshold((Bitmap)temp);
+            ot.threshold(temp, otsuThreshold);
+            textBox1.Text = otsuThreshold.ToString();
+            pictureBox2.Image = temp;
+
+        }
+
+        // критерий Ниблеска
+
+        void nibl(int a, float k, int ir) // ir 3 - ниблеска, ir 4 - сауволы
+        {
+            int i1, i2;
+            int j1, j2;
+            int f = a / 2;
+            int width = average.Width;
+            int height = average.Height;
+
+            for (int i = 0; i < height; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+                    float s2 = 0, m2 = 0, t = 0;
+                    float s = 0, m = 0;
+                    float d = 0, q = 0;
+                    float kol = 0;
+                    if ((i - f) <= 0) { i1 = 0; } else { i1 = i - f; };
+                    if ((i + f) >= h) { i2 = h - 1; } else { i2 = i + f; };
+                    if ((j - f) <= 0) { j1 = 0; } else { j1 = j - f; };
+                    if ((j + f) >= w) { j2 = w - 1; } else { j2 = j + f; };
+                    for (int i11 = i1; i11 <= i2; ++i11)
+                    {
+                        for (int j11 = j1; j11 <= j2; ++j11)
+                        {
+                            kol++;
+                            var pix1 = average.GetPixel(j11, i11);
+                            int gd1 = (int)(pix1.R);
+                            s += gd1; s2 += (float)Math.Pow(gd1, 2);
+                        }
+                    }
+                    m = s / kol; m2 = s2 / kol;
+                    d = m2 - (float)Math.Pow(m, 2);
+                    q = (float)Math.Sqrt(d);
+                    if (ir == 3) { t = (m + (k * q)); }
+                    if (ir == 4)
+                    {
+                        int r = 0;
+                        int clb = checkedListBox1.SelectedIndex;
+                        if (clb == 1 || clb == 3) { r = 256; }
+                        else { r = 128; }
+                        t = m * (1 + k * ((q / r) - 1));
+                    }
+
+                    var pix = average.GetPixel(j, i);
+                    int gd = (int)(pix.R);
+                    if (gd <= t) { gd = 0; }
+                    if (gd > t) { gd = 255; }
+                    finish.SetPixel(j, i, Color.FromArgb(gd, gd, gd));
+                }
+            }
+            pictureBox2.Image = finish;
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            float k = 0;
+            var NImg = start;
+            var w1 = NImg.Width;
+            var h1 = NImg.Height;
+            for (int i = 0; i < h1; ++i)
+            {
+                for (int j = 0; j < w1; ++j)
+                {
+                    var pix = start.GetPixel(j, i);
+                    int r = (int)(pix.R);
+                    int g = (int)(pix.G);
+                    int b = (int)(pix.B);
+                    int gd = 0;
+                    gd = (int)((0.2125 * r + 0.7154 * g + 0.0721 * b)) / 3;
+                    average.SetPixel(j, i, Color.FromArgb(gd, gd, gd));
+                }
+            }
+
+            //pictureBox2.Image = average;
+            nibl(3,4,3);
+        }
+
+
+    }
+}
